8634a02ce2b89c5bd4ac8b96825ef5b6a7436c07
SEHEE PARK <sehii0327@gmail.com>
2023-02-23 22:34:21 +0900

    [ADD] 로그인 기능 설정

--- Backend/git-rebase-test/app.js
+++ Backend/git-rebase-test/app.js
@@ -46,7 +46,29 @@ app.post('/users/signup', async(req,res)=> {
       ?,
       ?
     )`, [username, email, password]
-  )
+  )})
+app.post('/users/signin', async (req,res)=> {
+  const {email, password} = req.body;
+  const user = await appDataSource.query(
+    `SELECT
+    users.id,
+    users.password
+    FROM
+    users
+    WHERE
+    users.email = ?`,
+    [email]
+  );
+
+  if(!user) {
+    return res.json({message : "SIGNUP_REQUIRED"})
+  }
+
+  if(!(user[0].password === password)) {
+    return res.json({message: "INVALID_PASSWORD"})
+  };
+
+  return res.json({userId: user.id});
 })
 
 app.listen(PORT, () => {
